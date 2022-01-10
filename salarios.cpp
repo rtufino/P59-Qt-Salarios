1c9008cf895831d9c924e5182362088180bc4a7a
Rodrigo Tufino <rtufino@ups.edu.ec>
2022-01-10 10:55:12 -0500

    Calculos desde la GUI

--- salarios.cpp
+++ salarios.cpp
@@ -6,10 +6,33 @@ Salarios::Salarios(QWidget *parent)
     , ui(new Ui::Salarios)
 {
     ui->setupUi(this);
+    m_controlador = new Controlador();
 }
 
 Salarios::~Salarios()
 {
     delete ui;
+}
+
+
+void Salarios::on_cmdCalcular_clicked()
+{
+    // Obteber datos de la GUI
+    QString nombre = ui->inNombre->text();
+    int horas = ui->inHoras->value();
+    TipoJornada jornada;
+    if (ui->inMatutino->isChecked()){
+        jornada = TipoJornada::Matutina;
+    } else if (ui->inVespertina->isChecked()){
+        jornada = TipoJornada::Vespertina;
+    } else {
+        jornada = TipoJornada::Nocturna;
+    }
+    // Agregar obrero al controlador
+    m_controlador->agregarObrero(nombre, horas, jornada);
+    // Calcular
+    if (m_controlador->calcularSalario()){
+        ui->outResultado->appendPlainText(m_controlador->obrero()->toString());
+    }
 }
 
